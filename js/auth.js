65495010d52b6210cd579b9c5ea70e8c1e323c1a
addo212 <lada.lada21st@gmail.com>
2025-07-09 14:02:53 +0700

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,13 +1,3 @@
-// Initialize Firebase (paste your config here)
-const firebaseConfig = {
-  apiKey: "AIzaSyBSwh3Qj-rmHIbDFP63VY3omfjXa7XFqhw",
-  authDomain: "adlan-money-trancker.firebaseapp.com",
-  projectId: "adlan-money-trancker",
-  storageBucket: "adlan-money-trancker.appspot.com",
-  messagingSenderId: "686344739681",
-  appId: "1:686344739681:web:ad4fa48709e96c3ae30320"
-};
-
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 const auth = firebase.auth();
