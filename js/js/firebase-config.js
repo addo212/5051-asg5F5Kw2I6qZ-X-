5711f5435acfc4d62c7914159fe46b60403d1c64
addo212 <lada.lada21st@gmail.com>
2025-07-09 14:00:37 +0700

    Create firebase-config.js

---
+++ js/js/firebase-config.js
@@ -0,0 +1,12 @@
+// firebase-config.js
+const firebaseConfig = {
+  apiKey: "AIzaSyBSwh3Qj-rmHIbDFP63VY3omfjXa7XFqhw",
+  authDomain: "adlan-money-trancker.firebaseapp.com",
+  projectId: "adlan-money-trancker",
+  storageBucket: "adlan-money-trancker.appspot.com",
+  messagingSenderId: "686344739681",
+  appId: "1:686344739681:web:ad4fa48709e96c3ae30320"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
