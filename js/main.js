cc29404604cec96fb0ca956c40e8e943db95cff8
addo212 <lada.lada21st@gmail.com>
2025-07-10 20:18:40 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 // main.js - Dashboard functionality
 import { auth, initializeUserData, loadUserData, loadTransactions } from './database.js';
 import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-auth.js";
+import { formatRupiah } from './utils.js';
 
 // ============================================================================
 // Main Application Logic
